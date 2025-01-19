92132d956e3f7337ebf73e15c05263394dc56f44
fhjavinesjr <fhjavineswork@gmail.com>
2025-01-19 23:27:41 +0800

    Working with Time Shift module page 1st working Time Shift module page

---
+++ src/app/time-keeping/time-shift/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TimeShift from "./TimeShift"
+import Main from "../main/Main"
+
+export default function TimeShiftPage() {
+    return(
+        <Main>
+            <TimeShift />
+        </Main>
+    );
+}
